729214a926964fe2d8dbeb5ed2db3259e7225532
David Rubin <sinon@vortan.dev>
2026-06-03 16:18:29 -0700

    more optimizations

--- test.c
+++ test.c
@@ -1,15 +1,32 @@
-// `switch` with a shared (empty fall-through) label group and a default.
-// CHECK: foo(1)==10 && foo(2)==20 && foo(3)==20 && foo(7)==99
-int foo(int x) {
-    int r = 0;
-    switch (x) {
-        case 1: r = 10; break;
-        case 3: {
-          r = 20;
-          while (r > 0) {
-               r--;
-          }
-        }
-    }
-    return r;
+typedef unsigned long ulong;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+typedef union {
+    uchar bytes[32];
+    ulong limbs[4];
+} fd_uint256_t;
+
+void
+fd_ulong_add_carry4( ulong *l, uchar *h, ulong a0, ulong a1, ulong a2, uchar a3 ) {
+  ulong r0 = a0 + a1;
+  uchar c0 = r0 < a0;
+
+  ulong r1 = a2 + a3;
+  uchar c1 = r1 < a2;
+
+  *l = r0 + r1;
+  *h = (uchar)((*l < r0) + c0 + c1);
+}
+
+fd_uint256_t *
+fd_uint256_add(fd_uint256_t *       r,
+               fd_uint256_t const * a,
+               fd_uint256_t const * b ) {
+  uchar c0;
+  fd_ulong_add_carry4( &r->limbs[0], &c0, a->limbs[0], b->limbs[0], 0, 0 );
+  fd_ulong_add_carry4( &r->limbs[1], &c0, a->limbs[1], b->limbs[1], 0, c0 );
+  fd_ulong_add_carry4( &r->limbs[2], &c0, a->limbs[2], b->limbs[2], 0, c0 );
+  fd_ulong_add_carry4( &r->limbs[3], &c0, a->limbs[3], b->limbs[3], 0, c0 );
+  return r;
 }
